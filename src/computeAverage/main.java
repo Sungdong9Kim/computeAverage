a3c41f1959df1d54e50d17c75e7f0d1690cccc01
Sungdong9Kim <sungdong9.kim@gmail.com>
2021-02-14 18:27:40 +0900

    feat : init code for computing average in int array

---
+++ src/computeAverage/main.java
@@ -0,0 +1,12 @@
+package computeAverage;
+
+public class main {
+	public static void main(String[] args) {
+		AverageFinder finder =new AverageFinder();
+		
+		int[] testArray1 = {3, 7, 3};
+		System.out.println(finder.computeAverage(testArray1));
+	}
+
+
+}
